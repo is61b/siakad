7e83f082311c31fe84546da0fae3bfd00a249805
is61b <is61blp3i@gmail.com>
2024-01-04 10:34:10 +0700

    cek login

--- cek_login.php
+++ cek_login.php
@@ -7,4 +7,17 @@ if(!isset($_SESSION['sid']) AND !isset($_COOKIE['cid'])){
   </script>
   <?php
 }
+
+if(isset($_SESSION['sid'])){
+    //variabel elemt session
+    $l_id = $_SESSION['sid'];
+    $l_nama = $_SESSION['snama'];
+    $l_email = $_SESSION['semail'];
+
+}elseif(isset($_COOKIE['cid'])){
+    //variabel elemt cookie
+    $l_id = $_COOKIE['cid'];
+    $l_nama = $_COOKIE['cnama'];
+    $l_email = $_COOKIE['cemail'];
+}
 ?>
