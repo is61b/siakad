704afe6ed481919fa83bf3c765277b044392ade4
is61b <is61blp3i@gmail.com>
2024-01-04 09:51:55 +0700

    pembutaan cookie

--- login.php
+++ login.php
@@ -28,13 +28,21 @@ if(isset($_POST['tombol'])){
         $id = $data['id'];
         $nama = $data['nama'];
 
-        //pembuatan session
-        $_SESSION['sid'] = $id;
-        $_SESSION['snama'] = $nama;
-        $_SESSION['semail'] = $email;
+        if($_POST['ingat'] == "yes"){
+          //pembuatan cookie
+          setcookie("cid",$id, time() + (60*60*24*3), "/");
+          setcookie("cnama",$nama, time() + (60*60*24*3), "/");
+          setcookie("cemail",$email, time() + (60*60*24*3), "/");
+        }else{
+          //pembuatan session
+          $_SESSION['sid'] = $id;
+          $_SESSION['snama'] = $nama;
+          $_SESSION['semail'] = $email;
+        }
+        
 
         //update last log
-        $qry_update = "UPDATE users SET last_log='now()' 
+        $qry_update = "UPDATE users SET last_log=now() 
         WHERE id='$id'";
         $res_update = mysqli_query($con, $qry_update);
 
@@ -100,7 +108,7 @@ if(isset($_POST['tombol'])){
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
-              <input type="checkbox" id="remember">
+              <input type="checkbox" id="remember" name="ingat" value="yes">
               <label for="remember">
                 Remember Me
               </label>
