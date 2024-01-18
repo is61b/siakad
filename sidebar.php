96475f4190f50c97e3939cc5242474812b9369e1
is61b <is61blp3i@gmail.com>
2024-01-18 11:44:59 +0700

    tampil data jurusan

--- sidebar.php
+++ sidebar.php
@@ -25,7 +25,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="index.php" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
@@ -41,7 +41,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="jurusan.php" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Jurusan
